abc5efb48094d3ae3c8e58c4639d10e71624096e
1801-abhra <251030034@juitsolan.in>
2025-12-24 17:47:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-const path = require("path");
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 const dotenv = require("dotenv");
+const express = require("express");
 const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const { Server } = require("socket.io");
 
 dotenv.config();
+
 const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-cors: { origin: "*" }
+cors: {
+origin: "*",
+},
 });
+
+io.on("connection", (socket) => {
+console.log("Socket connected:", socket.id);
+
+socket.on("disconnect", () => {
+console.log("Socket disconnected:", socket.id);
+});
+});
+
+app.set("io", io);
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
+.catch((err) => console.error("Mongo error:", err));
+
 
 app.use("/api/auth", require("./src/routes/auth"));
-app.use("/api/rides", require("./src/routes/rides"));
+app.use("/api/rides", require("./src/routes/ride")); 
 
-io.on("connection", (socket) => {
-console.log("Socket connected:", socket.id);
+
+app.get("/", (req, res) => {
+res.send("Traverse backend running");
 });
-
-app.set("io", io);
 
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
